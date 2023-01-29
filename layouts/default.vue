d4e2d7479dbe14c8c10f3be7ecbc18089d0ba497
Gints Polis <polis.gints@gmail.com>
2023-01-29 15:21:55 +0200

    Adds login recirect to toolbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,22 @@
+<script lang="ts" setup>
+const currentUser = useAuthUser()
+</script>
+
 <template>
   <v-app>
     <v-layout>
       <v-app-bar>
         <template v-slot:prepend>
           <v-app-bar-nav-icon></v-app-bar-nav-icon>
-          <v-app-bar-title>Balcia insurance</v-app-bar-title>
         </template>
+
+        <v-app-bar-title>Insurer</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <NuxtLink v-if="!currentUser" to="/auth/login">
+          <v-btn icon>
+            <v-icon>mdi-login</v-icon>
+          </v-btn>
+        </NuxtLink>
       </v-app-bar>
 
       <v-navigation-drawer>Nav Bar</v-navigation-drawer>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  Hello from nuxt pages
+  Hello from nuxt pages, you have to <NuxtLink to="/auth/login">log in</NuxtLink> so we can track your insurance status.
 </template>
